d5a3e8c3fd854a03e5703a9c159d0fb1235dc0c4
soywiz <soywiz@gmail.com>
2024-08-11 10:30:06 +0200

    More work

--- src/main/kotlin/com/soywiz/SudokuSolver.kt
+++ src/main/kotlin/com/soywiz/SudokuSolver.kt
@@ -3,6 +3,41 @@ package com.soywiz
 class SudokuSolver(val board: SudokuBoard) {
     val cells = List(81) { SudokuCell(this, it) }
     val seqs = SudokuIndices.all.map { SudokuSequence(this, it) }
+    val squares = seqs.filter { it.indices.kind == SudokuIndices.Kind.SQUARE }
+    val cols = seqs.filter { it.indices.kind == SudokuIndices.Kind.COL }
+    val rows = seqs.filter { it.indices.kind == SudokuIndices.Kind.ROW }
+
+    fun isComplete() = board.isComplete()
+
+    operator fun get(index: Int) = cells[index]
+    operator fun get(col: Int, row: Int) = cells[row * 9 + col]
+
+    fun copyFrom(other: SudokuSolver) {
+        for (ncell in cells.indices) {
+            cells[ncell].copyFrom(other.cells[ncell])
+        }
+    }
+
+    fun clone() = SudokuSolver(board.clone()).also { it.copyFrom(this) }
+
+    fun solve(doInit: Boolean = true) {
+        if (doInit) solveInit()
+        while (true) {
+            printBoard()
+            val updateCount = updateStep()
+            println("updateCount=$updateCount")
+            if (updateCount == 0) {
+                if (isComplete()) {
+                    break
+                } else {
+                    for (cell in findEmptyCellsSortedByMissingCount()) {
+                        println("- $cell")
+                    }
+                    TODO()
+                }
+            }
+        }
+    }
 
     fun solveInit() {
         for (seq in seqs) seq.update()
@@ -10,11 +45,49 @@ class SudokuSolver(val board: SudokuBoard) {
     }
 
     fun findSingleCells(): List<SudokuCell> = cells.filter { it.missing.count == 1 && it.value == 0 }
+    fun findEmptyCells() = cells.filter { it.isEmpty }
+    fun findEmptyCellsSortedByMissingCount(): List<SudokuCell> = findEmptyCells().sortedBy { it.missing.count }
+
+    var step1Count = 0
+    var step2Count = 0
+    var step3Count = 0
 
     fun updateStep(): Int {
+        if (board.isComplete()) return 0
+        updateStep0().takeIf { it != 0 }?.let { step1Count++; return it }
+        if (board.isComplete()) return 0
+        updateStep1().takeIf { it != 0 }?.let { step2Count++; return it }
+        if (board.isComplete()) return 0
+        updateStep2().takeIf { it != 0 }?.let { step3Count++; return it }
+        if (board.isComplete()) return 0
+        TODO()
+    }
+
+    // Back-tracking
+    fun updateStep2(): Int {
+        val origin = this.clone()
+        val cellToTry = findEmptyCellsSortedByMissingCount().first()
+
+        for (v in cellToTry.missing.values()) {
+            try {
+                cellToTry.value = v
+                solve(doInit = false)
+                if (isComplete()) break
+                TODO()
+            } catch (e: IllegalStateException) {
+                this.copyFrom(origin)
+            }
+        }
+
+        return 1
+    }
+
+    // Same sequences
+    fun updateStep0(): Int {
         var count = 0
-        for (cell in findSingleCells()) {
-            println(cell)
+        val singleCells = findSingleCells()
+        for (cell in singleCells) {
+            //println(cell)
             val v = cell.missing.findFirstNum()
             cell.value = v
             cell.update()
@@ -22,6 +95,39 @@ class SudokuSolver(val board: SudokuBoard) {
                 seq.seq.update()
             }
             count++
+        }
+        return count
+    }
+
+    // Complementary sequences
+    fun updateStep1(): Int {
+        var count = 0
+        //val sorted = findEmptyCellsSortedByMissingCount()
+        //for (cell in sorted) {
+        //    println("cell=$cell")
+        //}
+
+        for (cell in findEmptyCells()) {
+            val cellMissing = cell.missing
+            //println("CELL=$cell")
+            for (seq in cell.seqs) {
+                var set = SudokuBitMask.ALL
+                for (cell2 in seq.findEmptyCells()) {
+                    if (cell2.cell == cell) continue
+                    set = set intersection cell2.cell.included
+                    //println("       - $cell2")
+                }
+                val intersection = cellMissing intersection set
+                if (intersection.count == 1) {
+                    val value = intersection.findFirstNum()
+                    //println("       - SETTING $value")
+                    cell.value = value
+                    count++
+                    //cell.updateAll()
+                    break
+                }
+                //println("!!! cellMissing=$cellMissing -- set=$set -- intersection=${cellMissing intersection set} ::: $seq")
+            }
         }
         return count
     }
@@ -43,7 +149,7 @@ class SudokuSolver(val board: SudokuBoard) {
 
     override fun toString(): String = StringBuilder(1024).also {
         var n = 0
-        it.appendLine("SudokuBoard(intArrayOf(")
+        it.appendLine("SudokuBoard(")
         for (x in 0 until 9) {
             it.append("  ")
             for (y in 0 until 9) {
@@ -54,11 +160,13 @@ class SudokuSolver(val board: SudokuBoard) {
             it.appendLine()
             if (x == 2 || x == 5) it.appendLine("  /*--------------------------------*/")
         }
-        it.appendLine("))")
+        it.appendLine(")")
     }.toString()
 }
 
 class SudokuCell(val solver: SudokuSolver, val index: Int) {
+    override fun equals(other: Any?): Boolean = other is SudokuCell && other.index == this.index
+
     val board get() = solver.board
 
     val ncol = index % 9
@@ -66,13 +174,35 @@ class SudokuCell(val solver: SudokuSolver, val index: Int) {
     //val ncol1 = ncol + 1
     //val nrow1 = nrow + 1
 
-    var value: Int
+    private var _value: Int
         get() = board.values[index]
         set(value) { board.values[index] = value }
+
+    var value: Int
+        get() = _value
+        set(value) {
+            _value = value
+            updateAll()
+        }
+
+    val isEmpty get() = value == 0
 
     val seqs = arrayListOf<SudokuSeqCell>()
     var missing = SudokuBitMask(0)
     var included = SudokuBitMask(0)
+
+    fun copyFrom(other: SudokuCell) {
+        this._value = other._value
+        this.missing = other.missing
+        this.included = other.included
+        for (nseq in seqs.indices) {
+            this.seqs[nseq].copyFrom(other.seqs[nseq])
+        }
+    }
+
+    fun validate() {
+        for (seq in seqs) seq.validate()
+    }
 
     fun update() {
         //missing = SudokuBitMask.ALL
@@ -85,6 +215,10 @@ class SudokuCell(val solver: SudokuSolver, val index: Int) {
         //println("missing[$index]=$missing")
     }
 
+    fun updateAll() {
+        for (s in seqs) s.seq.update()
+    }
+
     override fun toString(): String = "CELL($index[$ncol, $nrow], missing[${missing.count}]=$missing, included[${included.count}]=$included)"
 }
 
@@ -92,15 +226,44 @@ class SudokuSeqCell(val cell: SudokuCell, val seq: SudokuSequence, val seqIndex:
     var missing = SudokuBitMask(0)
     var included = SudokuBitMask(0)
 
+    var value: Int
+        get() = cell.value
+        set(value) {
+            cell.value = value
+        }
+
+    val isEmpty get() = cell.isEmpty
+
+    fun findEmptyCells() = seq.findEmptyCells()
+
+    fun copyFrom(other: SudokuSeqCell) {
+        this.missing = other.missing
+        this.included = other.included
+    }
+
     fun updated() {
         cell.update()
+    }
+
+    fun validate() {
+        seq.validate()
     }
 
     override fun toString(): String = "SudokuSeqCell[${seq.name}](${cell.index}, M=$missing, I=$included)"
 }
 
 inline class SudokuBoard(val values: IntArray) {
-    init { check(values.size == 81) }
+    constructor(vararg values: Int, unit: Unit = Unit) : this(values)
+
+    fun isComplete(): Boolean {
+        return values.all { it != 0 }
+    }
+
+    init {
+        check(values.size == 81)
+    }
+
+    fun clone() = SudokuBoard(values.copyOf())
 }
 
 class SudokuSequence(val solver: SudokuSolver, val indices: SudokuIndices) {
@@ -111,6 +274,10 @@ class SudokuSequence(val solver: SudokuSolver, val indices: SudokuIndices) {
             solver.cells[indices[it]].seqs += seqCell
         }
     }
+
+    fun findEmptyCells() = cells.filter { it.isEmpty }
+
+    override fun toString(): String = "SudokuSequence($cells)"
 
     operator fun get(n: Int) = board.values[indices[n]]
     fun getMask(): SudokuBitMask {
@@ -141,12 +308,20 @@ class SudokuSequence(val solver: SudokuSolver, val indices: SudokuIndices) {
         //println(includedMask.toString())
     }
 
+    fun validate() {
+        getMask()
+    }
+
     companion object {
         val INVALID = -1
     }
 }
 
-class SudokuIndices(val name: String, val indices: List<Int>) {
+class SudokuIndices(val kind: Kind, val index: Int, val indices: List<Int>) {
+    val name: String = "${kind.name.lowercase()}$index"
+
+    enum class Kind { ROW, COL, SQUARE }
+
     operator fun get(n: Int) = indices[n]
 
     companion object {
@@ -156,9 +331,9 @@ class SudokuIndices(val name: String, val indices: List<Int>) {
 
         val all = rows + cols + squares
 
-        fun row(n: Int) = SudokuIndices("row$n", List(9) { (n * 9) + it })
-        fun col(n: Int) = SudokuIndices("col$n", List(9) { n + (it * 9) })
-        fun square(n: Int) = SudokuIndices("square$n", List(9) {
+        fun row(n: Int) = SudokuIndices(Kind.ROW, n, List(9) { (n * 9) + it })
+        fun col(n: Int) = SudokuIndices(Kind.COL, n, List(9) { n + (it * 9) })
+        fun square(n: Int) = SudokuIndices(Kind.SQUARE, n, List(9) {
             val offset = ((n % 3) * 3) + ((n / 3) * 27)
             offset + ((it / 3) * 9) + (it % 3)
         })
@@ -173,15 +348,16 @@ inline class SudokuBitMask(val bits: Int) {
     }
 
     fun mask(value: Int): Int {
-        check(value in 1 .. 9)
+        check(value in 1..9)
         return 1 shl (value - 1)
     }
-    fun has(value: Int): Boolean = bits and mask(value) != 0
-    fun with(value: Int): SudokuBitMask = SudokuBitMask(bits or mask(value))
-    fun without(value: Int): SudokuBitMask = SudokuBitMask(bits and mask(value).inv())
+
+    infix fun has(value: Int): Boolean = bits and mask(value) != 0
+    infix fun with(value: Int): SudokuBitMask = SudokuBitMask(bits or mask(value))
+    infix fun without(value: Int): SudokuBitMask = SudokuBitMask(bits and mask(value).inv())
     fun inv(): SudokuBitMask = SudokuBitMask(bits.inv() and 0b111111111)
-    fun with(other: SudokuBitMask): SudokuBitMask = SudokuBitMask(this.bits or other.bits)
-    fun intersection(other: SudokuBitMask): SudokuBitMask = SudokuBitMask(this.bits and other.bits)
+    infix fun with(other: SudokuBitMask): SudokuBitMask = SudokuBitMask(this.bits or other.bits)
+    infix fun intersection(other: SudokuBitMask): SudokuBitMask = SudokuBitMask(this.bits and other.bits)
     val count: Int get() = bits.countOneBits()
     fun count(): Int = bits.countOneBits()
 
@@ -189,6 +365,8 @@ inline class SudokuBitMask(val bits: Int) {
         for (n in 1..9) if (has(n)) return n
         return -1
     }
+
+    fun values() = (1..9).filter { has(it) }
 
     override fun toString(): String {
         return StringBuilder().also {
--- src/test/kotlin/com/soywiz/SudokuSolverTest.kt
+++ src/test/kotlin/com/soywiz/SudokuSolverTest.kt
@@ -3,9 +3,12 @@ package com.soywiz
 import kotlin.test.*
 
 class SudokuSolverTest {
+    // REGLA 1: Mirar filas, columnas y cuadrados para una celda y ver si solo falta un número
+    // REGLA 2: ...
+
     @Test
-    fun test() {
-        val board = SudokuBoard(intArrayOf(
+    fun testLevel2() {
+        testSolve(SudokuBoard(
             0, 0, 7, /**/ 0, 6, 2, /**/ 3, 5, 0,
             0, 5, 1, /**/ 4, 0, 0, /**/ 6, 7, 0,
             3, 0, 0, /**/ 0, 0, 7, /**/ 0, 0, 0,
@@ -18,24 +21,76 @@ class SudokuSolverTest {
             9, 0, 0, /**/ 3, 4, 0, /**/ 0, 6, 0,
             7, 6, 0, /**/ 9, 0, 0, /**/ 4, 0, 5,
         ))
+    }
 
+    @Test
+    fun testLevel3() {
+        testSolve(SudokuBoard(
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 6, 3,
+            5, 9, 1, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 9, 2, /**/ 0, 0, 5,
+            ///////////////////////////////////
+            0, 0, 6, /**/ 0, 3, 1, /**/ 2, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            8, 0, 3, /**/ 0, 7, 9, /**/ 5, 0, 0,
+            ///////////////////////////////////
+            0, 0, 5, /**/ 7, 0, 0, /**/ 0, 0, 9,
+            3, 0, 0, /**/ 2, 8, 0, /**/ 6, 7, 0,
+            0, 4, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+        ))
+    }
+
+    @Test
+    fun testLevel3b() {
+        val board = SudokuBoard(
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 6, 3,
+            5, 9, 1, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 9, 2, /**/ 0, 0, 5,
+            /*--------------------------------*/
+            0, 0, 6, /**/ 0, 3, 1, /**/ 2, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            8, 2, 3, /**/ 0, 7, 9, /**/ 5, 0, 0,
+            /*--------------------------------*/
+            0, 0, 5, /**/ 7, 0, 0, /**/ 0, 0, 9,
+            3, 1, 9, /**/ 2, 8, 5, /**/ 6, 7, 4,
+            0, 4, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+        )
         val solver = SudokuSolver(board)
-        solver.solveInit()
-        for (n in 0 until 10) {
-            solver.printBoard()
-            val updateCount = solver.updateStep()
-            println("updateCount=$updateCount")
-            if (updateCount == 0) break
-        }
+        solver.solve()
+    }
 
-        //println(solver.cells[19].seqs)
+    @Test
+    fun testExtremo() {
+        testSolve(SudokuBoard(
+            0, 0, 2, /**/ 0, 0, 5, /**/ 8, 3, 0,
+            7, 0, 0, /**/ 0, 2, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 4, 0,
+            ///////////////////////////////////
+            0, 0, 1, /**/ 2, 0, 7, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 9, 0, 0,
+            0, 8, 0, /**/ 1, 0, 3, /**/ 4, 0, 0,
+            ///////////////////////////////////
+            0, 9, 0, /**/ 8, 0, 0, /**/ 0, 0, 0,
+            6, 0, 0, /**/ 0, 5, 0, /**/ 0, 0, 8,
+            0, 3, 0, /**/ 7, 0, 0, /**/ 0, 0, 6,
+        ))
+    }
 
-        //for (cel in solver.cells) if (cel.missing.count == 1) println(cel)
-        //println(solver.cells[0])
-
-        //println(SudokuIndices.rows)
-        //println(SudokuIndices.cols)
-        //println(SudokuIndices.squares)
+    @Test
+    fun testEmpty() {
+        testSolve(SudokuBoard(
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            ///////////////////////////////////
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            ///////////////////////////////////
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+            0, 0, 0, /**/ 0, 0, 0, /**/ 0, 0, 0,
+        ))
     }
 
     @Test
@@ -47,5 +102,19 @@ class SudokuSolverTest {
         assertEquals(81, SudokuIndices.rows.map { it.indices }.flatten().distinct().size)
         assertEquals(81, SudokuIndices.cols.map { it.indices }.flatten().distinct().size)
         assertEquals(81, SudokuIndices.squares.map { it.indices }.flatten().distinct().size)
+    }
+
+    private fun testSolve(board: SudokuBoard) {
+        val solver = SudokuSolver(board)
+        solver.solve()
+
+        //println(solver.cells[19].seqs)
+
+        //for (cel in solver.cells) if (cel.missing.count == 1) println(cel)
+        //println(solver.cells[0])
+
+        //println(SudokuIndices.rows)
+        //println(SudokuIndices.cols)
+        //println(SudokuIndices.squares)
     }
 }
